24de63ef08ebb927878fc2e91148c292aadcba75
Charlie Tighe <ctighe3@gatech.edu>
2024-04-05 15:42:10 -0400

    Started on GLFB, but paper is a little vague about some aspects. I will need to look into the inner layers a little more.

--- modules/GLFB.py
+++ modules/GLFB.py
@@ -3,20 +3,55 @@ import torchaudio
 import torch.nn as nn
 import unittest
 
-class GFLB(nn.Module):
+class GLFB(nn.Module):
     """
 
     """
     
-    def __init__(self):
-        super(GFLB, self).__init__()
+    def __init__(self, in_channels):
+        super(GLFB, self).__init__()
+        self.glfb_layer_1 = nn.Sequential(
+            # Layer Norm
+            nn.Conv2d(  # Point Conv 1 & 3 double number of channels (https://arxiv.org/pdf/2306.04286.pdf Section 2.4)
+                in_channels = in_channels,
+                out_channels = in_channels * 2,
+                kernel_size = 1
+            ),
+            # Depth Wise Seperable Convolution
+            # Gate
+            # Channel Attention
+            nn.Conv2d(  # Point Conv 2 & 4 halve number of channels (https://arxiv.org/pdf/2306.04286.pdf Section 2.4)
+                in_channels = in_channels * 2,
+                out_channels = in_channels,
+                kernel_size = 1
+            )
+        )
+        self.glfb_layer_2 = nn.Sequential(
+            # Layer Norm
+            nn.Conv2d(  # Point Conv 1 & 3 double number of channels (https://arxiv.org/pdf/2306.04286.pdf Section 2.4)
+                in_channels = in_channels,
+                out_channels = in_channels * 2,
+                kernel_size = 1
+            ),
+            # Gate
+            nn.Conv2d(  # Point Conv 2 & 4 halve number of channels (https://arxiv.org/pdf/2306.04286.pdf Section 2.4)
+                in_channels = in_channels * 2,
+                out_channels = in_channels,
+                kernel_size = 1
+            )
+        )
         
     def forward(self, x):
-        pass
+        l1 = self.glfb_layer_1(x)
+        y = torch.add(l1, x)
+        l2 = self.glfb_layer_2(y)
+        out = torch.add(l2, y)
+        return out
+        
     
     
-class TestUpSample(unittest.TestCase):
-    def test_gflb(self):
+class TestGLFB(unittest.TestCase):
+    def test_glfb(self):
         pass
 
         
