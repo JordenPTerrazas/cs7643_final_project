94825f7b467965d58365bffd87c52703e7280fcd
JordenPTerrazas <jordenpterrazas@gmail.com>
2024-04-06 14:21:46 -0500

    Added Layer norm, depth wise seperable, and channel attention. Changed point wise conv 2 and 4 a bit, I think the halving is supposed to be in the gate

--- modules/GLFB.py
+++ modules/GLFB.py
@@ -2,40 +2,61 @@ import torch
 import torchaudio
 import torch.nn as nn
 import unittest
+from ChannelAttention import ChannelAttention
 
 class GLFB(nn.Module):
     """
 
     """
-    
-    def __init__(self, in_channels):
+    def __init__(self, in_channels, reduction_ratio):
         super(GLFB, self).__init__()
         self.glfb_layer_1 = nn.Sequential(
             # Layer Norm
+            nn.LayerNorm(in_channels),
             nn.Conv2d(  # Point Conv 1 & 3 double number of channels (https://arxiv.org/pdf/2306.04286.pdf Section 2.4)
                 in_channels = in_channels,
                 out_channels = in_channels * 2,
                 kernel_size = 1
             ),
-            # Depth Wise Seperable Convolution
-            # Gate
-            # Channel Attention
-            nn.Conv2d(  # Point Conv 2 & 4 halve number of channels (https://arxiv.org/pdf/2306.04286.pdf Section 2.4)
+            # Depth Wise Seperable Convolution (https://www.paepper.com/blog/posts/depthwise-separable-convolutions-in-pytorch/)
+            nn.Conv2d( # From what I can tell no mention is made as to the kernel size or padding used for the depth wise seperable convolution
                 in_channels = in_channels * 2,
+                out_channels = in_channels * 2,
+                kernel_size = 3,
+                groups = in_channels * 2,
+            ),
+            # Gate (Not many details are given in the paper, so we'll assume a simple sigmoid gate for now) 
+            nn.Conv2d(  # Gating is stated to halve the number of channels (https://arxiv.org/pdf/2306.04286.pdf Section 2.4)
+                in_channels = in_channels * 2, 
+                out_channels = in_channels, 
+                kernel_size = 1
+            ),
+            nn.Sigmoid(),
+            # Channel Attention
+            ChannelAttention(in_channels = in_channels, reduction_ratio = reduction_ratio),
+            nn.Conv2d(  # Point Conv 2 & 4 maintain number of channels (https://arxiv.org/pdf/2306.04286.pdf Section 2.4)
+                in_channels = in_channels,
                 out_channels = in_channels,
                 kernel_size = 1
             )
         )
         self.glfb_layer_2 = nn.Sequential(
             # Layer Norm
+            nn.LayerNorm(in_channels),
             nn.Conv2d(  # Point Conv 1 & 3 double number of channels (https://arxiv.org/pdf/2306.04286.pdf Section 2.4)
                 in_channels = in_channels,
                 out_channels = in_channels * 2,
                 kernel_size = 1
             ),
-            # Gate
-            nn.Conv2d(  # Point Conv 2 & 4 halve number of channels (https://arxiv.org/pdf/2306.04286.pdf Section 2.4)
+            # Gate (Not many details are given in the paper, so we'll assume a simple sigmoid gate for now) 
+            nn.Conv2d(  # Gating is stated to halve the number of channels (https://arxiv.org/pdf/2306.04286.pdf Section 2.4)
                 in_channels = in_channels * 2,
+                out_channels = in_channels, 
+                kernel_size = 1
+            ),
+            nn.Sigmoid(),
+            nn.Conv2d(  # Point Conv 2 & 4 maintain number of channels (https://arxiv.org/pdf/2306.04286.pdf Section 2.4)
+                in_channels = in_channels,
                 out_channels = in_channels,
                 kernel_size = 1
             )
@@ -49,7 +70,7 @@ class GLFB(nn.Module):
         return out
         
     
-    
+
 class TestGLFB(unittest.TestCase):
     def test_glfb(self):
         pass
