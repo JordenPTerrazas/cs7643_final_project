cab9a4cdecc1c5942d7a46ea9f31df6ea4277ccb
Charlie Tighe <ctighe3@gatech.edu>
2024-04-13 18:04:54 -0400

    Thanks for repushing. You're right, there must have been a merge conflict. That fixed the shape issue for me.

--- modules/GLFB.py
+++ modules/GLFB.py
@@ -4,7 +4,6 @@ import torch.nn as nn
 import unittest
 from ChannelAttention import ChannelAttention
 
-
 class GLFB(nn.Module):
     """
     Global Local Former Block implemented as described in 'A Mask Free Neural Network 
@@ -14,69 +13,57 @@ class GLFB(nn.Module):
     """
     def __init__(self, in_channels, reduction_ratio, frame_size, n_frames):
         super(GLFB, self).__init__()
-        
-        # Different LayerNorms so that different parameters can be learned
-        self.norm_1 = nn.LayerNorm([in_channels, frame_size, n_frames])
-        self.norm_2 = nn.LayerNorm([in_channels, frame_size, n_frames])
-        
-        # Point Conv 1 & 3 double number of channels (https://arxiv.org/pdf/2306.04286.pdf Section 2.4)
-        # Point Conv 2 & 4 maintain number of channels (https://arxiv.org/pdf/2306.04286.pdf Section 2.4)
-        self.point_conv_1 = nn.Conv2d(  
-            in_channels = in_channels,
-            out_channels = in_channels * 2,
-            kernel_size = 1
-        ) 
-        self.point_conv_2 = nn.Conv2d(  
-            in_channels = in_channels,
-            out_channels = in_channels,
-            kernel_size = 1
-        )
-        self.point_conv_3 = nn.Conv2d(
-            in_channels = in_channels,
-            out_channels = in_channels * 2,
-            kernel_size = 1
-        )
-        self.point_conv_4 = nn.Conv2d(
-            in_channels = in_channels,
-            out_channels = in_channels,
-            kernel_size = 1
-        )
-        
-        self.dw_conv = nn.Conv2d(
-            in_channels = in_channels * 2,
-            out_channels = in_channels * 2,
-            kernel_size = 1,    # Kernel size for DW Conv not specified in Paper.
-            padding = "same",   # If we change kernel size, will pad input so that output shape remains the same.
-            groups = in_channels * 2    # DW Conv has each input channel convolved separately
-        )
-        
-        # Not much information as to how to implement Gate Block in Paper.
-        # Assume a GLU is correct, but paper mentions not using any activation functions,
-        # which are a part of GLU, so I'm not 100% sure.
-        self.gate_1 = nn.GLU(dim = 1)
-        self.gate_2 = nn.GLU(dim = 1)
-        
-        self.attention = ChannelAttention(
-            in_channels = in_channels, 
-            reduction_ratio = reduction_ratio
-        )
-        
         self.glfb_layer_1 = nn.Sequential(
-            self.norm_1,
-            self.point_conv_1,
-            self.dw_conv,
-            self.gate_1,
-            self.attention,
-            self.point_conv_2
+            # Layer Norm
+            nn.LayerNorm((in_channels, frame_size, n_frames)),
+            nn.Conv2d(  # Point Conv 1 & 3 double number of channels (https://arxiv.org/pdf/2306.04286.pdf Section 2.4)
+                in_channels = in_channels,
+                out_channels = in_channels * 2,
+                kernel_size = 1
+            ),
+            # Depth Wise Seperable Convolution (https://www.paepper.com/blog/posts/depthwise-separable-convolutions-in-pytorch/)
+            nn.Conv2d( # From what I can tell no mention is made as to the kernel size or padding used for the depth wise seperable convolution
+                in_channels = in_channels * 2,
+                out_channels = in_channels * 2,
+                kernel_size = 1,
+                groups = in_channels * 2,
+            ),
+            # Gate (Not many details are given in the paper, so we'll assume a simple sigmoid gate for now) 
+            nn.Conv2d(  # Gating is stated to halve the number of channels (https://arxiv.org/pdf/2306.04286.pdf Section 2.4)
+                in_channels = in_channels * 2, 
+                out_channels = in_channels, 
+                kernel_size = 1
+            ),
+            nn.Sigmoid(),
+            # Channel Attention
+            ChannelAttention(in_channels = in_channels, reduction_ratio = reduction_ratio),
+            nn.Conv2d(  # Point Conv 2 & 4 maintain number of channels (https://arxiv.org/pdf/2306.04286.pdf Section 2.4)
+                in_channels = in_channels,
+                out_channels = in_channels,
+                kernel_size = 1
+            )
         )
-        
         self.glfb_layer_2 = nn.Sequential(
-            self.norm_2,
-            self.point_conv_3,
-            self.gate_2,
-            self.point_conv_4
+            # Layer Norm
+            nn.LayerNorm((in_channels, frame_size, n_frames)),
+            nn.Conv2d(  # Point Conv 1 & 3 double number of channels (https://arxiv.org/pdf/2306.04286.pdf Section 2.4)
+                in_channels = in_channels,
+                out_channels = in_channels * 2,
+                kernel_size = 1
+            ),
+            # Gate (Not many details are given in the paper, so we'll assume a simple sigmoid gate for now) 
+            nn.Conv2d(  # Gating is stated to halve the number of channels (https://arxiv.org/pdf/2306.04286.pdf Section 2.4)
+                in_channels = in_channels * 2,
+                out_channels = in_channels, 
+                kernel_size = 1
+            ),
+            nn.Sigmoid(),
+            nn.Conv2d(  # Point Conv 2 & 4 maintain number of channels (https://arxiv.org/pdf/2306.04286.pdf Section 2.4)
+                in_channels = in_channels,
+                out_channels = in_channels,
+                kernel_size = 1
+            )
         )
-        
         
     def forward(self, x):
         l1 = self.glfb_layer_1(x)
@@ -85,23 +72,13 @@ class GLFB(nn.Module):
         out = torch.add(l2, y)
         return out
         
-    
-    
 class TestGLFB(unittest.TestCase):
-    def test_glfb_shape(self):
-        batch_size, in_channels, frame_size, n_frames = 3, 2, 4, 4
-        input = torch.rand((batch_size, in_channels, frame_size, n_frames))
-        model = GLFB(in_channels, frame_size, n_frames)
-        output = model.forward(input)
-        N, C, F, S = output.shape
-        self.assertTrue(
-            (N == batch_size) 
-            and (C == in_channels) 
-            and (F == frame_size) 
-            and (S == n_frames)
-        )
+    def test_glfb(self):
+        glfb = GLFB(in_channels = 1, reduction_ratio = 8)
+        x = torch.randn(1, 320, 999)
+        out = glfb(x)
+        print(out.size())
+        pass
 
-        
-        
 if __name__ == "__main__":
     unittest.main()
